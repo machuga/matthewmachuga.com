9929ddd4c52a0f309e499d201a33e1e8b641387d
Matt Machuga <machuga@gmail.com>
2023-05-07 15:07:20 -0400

    Updating website and removing contact form. Too much spam

--- Gemfile
+++ Gemfile
@@ -1,17 +1,19 @@
 source 'https://rubygems.org'
 
-gem 'middleman', '~> 4.2'
+gem 'middleman', '~> 4.4'
 gem 'middleman-autoprefixer', '~> 2.7'
-gem "middleman-blog", "~> 4.0"
-gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
-gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw]
+gem 'middleman-blog', '~> 4.0.3'
+# gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
+# gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw]
 
 # For feed.xml.builder
-gem "builder", "~> 3.0"
-gem "nokogiri"
+gem 'builder', '~> 3.0'
+gem 'nokogiri'
 
 # Syntax Highlighting
-gem "middleman-syntax"
-gem "redcarpet"
+gem 'middleman-livereload'
+gem 'middleman-syntax', '~>3.3.0'
+gem 'redcarpet', '~>3.6.0'
 
-
+# Temporary fix for https://github.com/middleman/middleman/issues/2569
+gem 'haml', '~> 5.0'
--- config.rb
+++ config.rb
@@ -5,7 +5,7 @@ set :markdown, fenced_code_blocks: true, smartypants: true
 activate :syntax, line_numbers: false
 
 activate :autoprefixer do |prefix|
-  prefix.browsers = "last 2 versions"
+  prefix.browsers = 'last 2 versions'
 end
 
 # Layouts
@@ -16,29 +16,28 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-page "blog/feed.xml", layout: false
+page 'blog/feed.xml', layout: false
 
 activate :blog do |blog|
-  blog.prefix = "blog"
-  blog.layout = "blog"
-  blog.permalink = "{year}/{title}.html"
-  blog.sources = "{year}/{title}.html"
-  blog.taglink = "tags/{tag}.html"
-  #blog.summary_separator = /(READMORE)/
+  blog.prefix = 'blog'
+  blog.layout = 'blog'
+  blog.permalink = '{year}/{title}.html'
+  blog.sources = '{year}/{title}.html'
+  blog.taglink = 'tags/{tag}.html'
+  # blog.summary_separator = /(READMORE)/
   blog.summary_length = 75
-  blog.year_link = "{year}.html"
-  #blog.month_link = "{year}/{month}.html"
-  #blog.day_link = "{year}/{month}/{day}.html"
+  blog.year_link = '{year}.html'
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
 
-  blog.tag_template = "blog/tag.html"
-  blog.calendar_template = "blog/calendar.html"
+  blog.tag_template = 'blog/tag.html'
+  blog.calendar_template = 'blog/calendar.html'
 
   # Enable pagination
   blog.paginate = true
   blog.per_page = 25
-  blog.page_link = "page/{num}"
+  blog.page_link = 'page/{num}'
 end
-
 
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
@@ -59,13 +58,13 @@ end
 # https://middlemanapp.com/basics/helper-methods/
 
 helpers do
-	def site_info
-		data.site_info
-	end
+  def site_info
+    data.site_info
+  end
 
-	def current_title
-		current_article && current_article.title || current_page.data.title
-	end
+  def current_title
+    current_article&.title || current_page.data.title
+  end
 end
 
 # Build-specific configuration
